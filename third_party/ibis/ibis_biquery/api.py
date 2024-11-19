5df5246e2555a683f1e1a2e0548b1a8679fe9657
Neil Johnson <neiljohnson@google.com>
2024-11-19 17:08:34 +0000

    Update third_party/ibis/ibis_biquery/api.py
    Co-authored-by: Helen Cristina <helencristina@google.com>

--- third_party/ibis/ibis_biquery/api.py
+++ third_party/ibis/ibis_biquery/api.py
@@ -3,7 +3,7 @@ from ibis.backends.bigquery import Backend as BigQueryBackend
 
 def _list_primary_key_columns(self, database: str, table: str) -> list:
     """Return a list of primary key column names."""
-    # TODO nj@2024-11-08 It's not clear if this is possible, we should revisit if it becomes a requirement.
+    # TODO: Related to issue-1253, it's not clear if this is possible, we should revisit if it becomes a requirement.
     return None
 
 
